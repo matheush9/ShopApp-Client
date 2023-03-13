0c2f5fca2efdac551524ab76b3dcfe360fcd5162
Matheus Henrique <matheushenr941@gmail.com>
2023-03-12 22:45:11 -0300

    feat: added core module
    moved app.module imports to here

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginComponent } from './auth/components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    SharedModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
+  ],
+})
+export class CoreModule {}
