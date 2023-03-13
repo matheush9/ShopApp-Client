c45e6502df010fc20c7a6d4455b3b214637b82ee
Matheus Henrique <matheushenr941@gmail.com>
2023-03-12 22:43:40 -0300

    refactor: removing wrong imports from app.module
    unnecessary material modules and core compoenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatExpansionModule } from '@angular/material/expansion';
-
-import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { FooterComponent } from './core/components/footer/footer.component';
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatInputModule,
+    CoreModule,
     SharedModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
