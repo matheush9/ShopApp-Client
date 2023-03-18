af87bb4c0edce2b576ffe1b1a5bbc10cfb74567b
Matheus Henrique <matheushenr941@gmail.com>
2023-03-18 14:39:10 -0300

    feat: product-check filter component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,8 +4,11 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './components/input/input.component';
 import { IconBoxComponent } from './components/icon-box/icon-box.component';
 import { ProductCardComponent } from './components/product-card/product-card/product-card.component';
+import { ProductCheckFilterComponent } from './components/product-filter/components/product-check-filter/product-check-filter.component';
 
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 
@@ -13,16 +16,20 @@ import { MatIconModule } from '@angular/material/icon';
   declarations: [
     InputComponent,
     IconBoxComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    ProductCheckFilterComponent
   ],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
+    MatCheckboxModule
   ],
   exports: [
     InputComponent,
     IconBoxComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    ProductCheckFilterComponent,
   ]
 })
 export class SharedModule { }
