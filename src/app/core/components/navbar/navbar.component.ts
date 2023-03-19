e8539c324917adc14f06f2dcd9fa01e6cf8d3578
Matheus Henrique <matheushenr941@gmail.com>
2023-03-19 11:08:57 -0300

    feat: on nav menu open, lock page scroll

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  constructor(private renderer: Renderer2) {}
 
+  onOpenMenuClick() {
+    this.renderer.addClass(document.body, 'block-scroll');
+  }
 }
