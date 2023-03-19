2d4658030dc636e61b9edf99324cb13ca2de107c
Matheus Henrique <matheushenr941@gmail.com>
2023-03-19 12:23:06 -0300

    fix: remove scroll lock when menu close

--- src/app/features/components/product-listing/product-listing.component.ts
+++ src/app/features/components/product-listing/product-listing.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-product-listing',
   templateUrl: './product-listing.component.html',
-  styleUrls: ['./product-listing.component.scss']
+  styleUrls: ['./product-listing.component.scss'],
 })
 export class ProductListingComponent {
+  constructor(private renderer: Renderer2) {}
+
+  onOpenMenuClick() {
+    this.checked = true;
+    this.renderer.addClass(document.body, 'block-scroll');
+  }
+
+  onCloseMenuClick() {
+    this.renderer.removeClass(document.body, 'block-scroll');
+  }
 }
