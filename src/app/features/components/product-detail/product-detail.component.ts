6113ce951cc6fbcc6df167d793ba539372e6821a
Matheus Henrique <matheushenr941@gmail.com>
2023-03-22 22:30:32 -0300

    feat: product-detail component

---
+++ src/app/features/components/product-detail/product-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss'],
+})
+export class ProductDetailComponent {
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    nav: false,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 600,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  };
+}
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListingComponent } from './components/product-listing/product-listing.component';
+import { ProductFilterGroupComponent } from './components/product-filter-group/product-filter-group/product-filter-group.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 import { SharedModule } from '../shared/shared.module';
-import { ProductFilterGroupComponent } from './components/product-filter-group/product-filter-group/product-filter-group.component';
+
 
 @NgModule({
   declarations: [
@@ -20,10 +23,12 @@ import { ProductFilterGroupComponent } from './components/product-filter-group/p
     CommonModule,
     SharedModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    CarouselModule 
   ],
   exports: [
-    ProductListingComponent
+    ProductListingComponent,
+    ProductDetailComponent
   ]
 })
 export class FeaturesModule { }
