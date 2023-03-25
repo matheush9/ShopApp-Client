781ff4740f8b0f03795708460b52087afa95b541
Matheus Henrique <matheushenr941@gmail.com>
2023-03-25 10:00:43 -0300

    feat: added store-page component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,20 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListingComponent } from './components/product-listing/product-listing.component';
 import { ProductFilterGroupComponent } from './components/product-filter-group/product-filter-group/product-filter-group.component';
+import { SharedModule } from '../shared/shared.module';
+import { StorePageComponent } from './components/store-page/store-page.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
-import { SharedModule } from '../shared/shared.module';
-
 
 @NgModule({
   declarations: [
     ProductDetailComponent,
     ProductListingComponent,
-    ProductFilterGroupComponent
+    ProductFilterGroupComponent,
+    StorePageComponent
   ],
   imports: [
     CommonModule,
@@ -28,7 +29,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   exports: [
     ProductListingComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    StorePageComponent
   ]
 })
 export class FeaturesModule { }
