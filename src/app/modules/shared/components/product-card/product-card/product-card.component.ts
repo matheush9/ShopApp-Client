d5f034937a944c50aaf857d1c4c49127390a6c89
Matheus Henrique <matheushenr941@gmail.com>
2023-04-01 22:35:07 -0300

    feat: product card, use input data

--- src/app/modules/shared/components/product-card/product-card/product-card.component.ts
+++ src/app/modules/shared/components/product-card/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
-
+  @Input() productName: string = '';
+  @Input() productImageUrl: string = '/assets/svg/logo.svg';
 }
