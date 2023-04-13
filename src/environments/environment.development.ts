b50bfdb79c5158539d9e7f616f2099d20b143142
Matheus Henrique <matheushenr941@gmail.com>
2023-04-12 23:15:22 -0300

    feat: added imagesproviderUrl property in environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7240/api'
+    apiUrl: 'https://localhost:7240/api',
+    imagesProviderUrl: 'https://localhost:7240'
 };
