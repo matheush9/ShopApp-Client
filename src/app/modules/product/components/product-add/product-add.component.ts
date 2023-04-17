e694f142de79c1bc3c19b6e72146de9e3b00ec80
Matheus Henrique <matheushenr941@gmail.com>
2023-04-17 01:06:35 -0300

    feat: product add component: load product  and image

--- src/app/modules/product/components/product-add/product-add.component.ts
+++ src/app/modules/product/components/product-add/product-add.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, catchError, tap, of } from 'rxjs';
+
+import { Product } from '../../interfaces/product-interface';
+import { Image } from 'src/app/modules/shared/interfaces/image-interface';
+import { ProductService } from '../../services/product.service';
+import { ImageService } from 'src/app/modules/shared/services/image.service';
 
 import { OwlOptions } from 'ngx-owl-carousel-o';
-import {
-  MatDialog,
-  MatDialogRef,
-  MatDialogConfig,
-} from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
 import { ConfirmationDialogComponent } from 'src/app/modules/shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
@@ -15,8 +19,21 @@ import { ConfirmationDialogComponent } from 'src/app/modules/shared/components/c
 })
 export class ProductAddComponent {
   newProduct: boolean = false;
+  product$?: Observable<Product>;
+  product?: Product;
+  images: Image[] = [];
+  images$?: Observable<Image[]>;
+  error$ = new Subject<boolean>();
+  imagesProviderUrl: string;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(
+    public dialog: MatDialog,
+    private productService: ProductService,
+    private imageService: ImageService,
+    private route: ActivatedRoute
+  ) {
+    this.imagesProviderUrl = this.imageService.getImagesProviderUrl();
+  }
 
   customOptions: OwlOptions = {
     loop: false,
@@ -36,10 +53,23 @@ export class ProductAddComponent {
     },
   };
 
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        tap((params) => {
+          const id = params.get('id');
+          console.log(id);
+          this.getProduct(Number(id));
+          this.getImagesByProduct(Number(id));
+        })
+      )
+      .subscribe();
+  }
+
   openDialog(
     enterAnimationDuration: string,
     exitAnimationDuration: string,
-    dialogMessage: string,
+    dialogMessage: string
   ): void {
     const dialogConfig = new MatDialogConfig();
 
@@ -54,5 +84,26 @@ export class ProductAddComponent {
         message: dialogMessage,
       },
     });
+  }
+
+  getProduct(productId: number) {
+    this.product$ = this.productService.getProduct(productId).pipe(
+      catchError((error) => {
+        console.error(error);
+        this.error$.next(true);
+        return of();
+      }),
+      tap((data) => (this.product = data))
+    );
+  }
+
+  getImagesByProduct(productId: number) {
+    this.images$ = this.imageService.getImagesbyProduct(productId).pipe(
+      catchError((error) => {
+        console.error(error);
+        this.error$.next(true);
+        return of();
+      })
+    );
   }
 }
