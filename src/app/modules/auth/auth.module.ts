56171468903772bf01dd4250a68f4a7807e62bb6
Matheus Henrique <matheushenr941@gmail.com>
2023-04-26 23:00:37 -0300

    feat: login component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -24,6 +25,7 @@ import { MatRadioModule } from '@angular/material/radio';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     AuthRoutingModule,
     MatFormFieldModule,
     MatInputModule,
--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+import { Subject, catchError, of } from 'rxjs';
+
+import { LoginService } from '../../services/login.service';
+import { JwtTokenService } from './../../../shared/services/jwt-token.service';
+import { User } from '../../interfaces/user-interface';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  error$ = new Subject<boolean>();
+  user: User = {
+    id: 0,
+    name: '',
+    email: '',
+    password: '',
+  };
 
+  constructor(
+    private loginService: LoginService,
+    private jwtTokenService: JwtTokenService
+  ) {}
+
+  login() {
+    this.loginService
+      .authenticateUser(this.user)
+      .pipe(
+        catchError((error) => {
+          console.error(error);
+          this.error$.next(true);
+          return of();
+        })
+      )
+      .subscribe((tokenObj) => {
+        this.storeToken(tokenObj.token);
+      });
+  }
+
+  storeToken(token: string) {
+    this.jwtTokenService.setToken(token);
+  }
 }
