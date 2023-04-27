cabc3e59723ca60a20424dda9b997ef7d1b1bbca
Matheus Henrique <matheushenr941@gmail.com>
2023-04-26 23:00:55 -0300

    feat: added auth headers in product service

--- src/app/modules/product/services/product.service.ts
+++ src/app/modules/product/services/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { Product } from '../interfaces/product-interface';
+import { JwtTokenService } from './../../shared/services/jwt-token.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,23 +12,34 @@ import { Product } from '../interfaces/product-interface';
 export class ProductService {
   private apiUrl?: string;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(
+    private httpClient: HttpClient,
+    private jwtTokenService: JwtTokenService
+  ) {
     this.apiUrl = environment.apiUrl;
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.httpClient.get<Product>(this.apiUrl + '/Product/' + id);
+    return this.httpClient.get<Product>(this.apiUrl + '/Product/' + id, {
+      headers: this.getAuthHeader(),
+    });
   }
 
   searchProduct(query: string): Observable<Product[]> {
     return this.httpClient.get<Product[]>(
-      this.apiUrl + '/Product/search/' + query
+      this.apiUrl + '/Product/search/' + query, {headers: this.getAuthHeader()}
     );
   }
 
   filterProductsByParams(params: HttpParams): Observable<Product[]> {
-    return this.httpClient.get<Product[]>(
-      this.apiUrl + '/Product/filter/', {params: params}
-    );
+    return this.httpClient.get<Product[]>(this.apiUrl + '/Product/filter/', {
+      params: params, headers: this.getAuthHeader(),
+    });
+  }
+  
+  getAuthHeader() {
+    return new HttpHeaders({
+      Authorization: 'Bearer ' + this.jwtTokenService.getToken(),
+    });
   }
 }
