e89cc4b7308d88084e5dc0846fd572bf2e3c8647
Matheus Henrique <matheushenr941@gmail.com>
2023-04-30 23:58:06 -0300

    feat: quantity selector amount handling

--- src/app/modules/shared/components/quantity-selector/quantity-selector.component.ts
+++ src/app/modules/shared/components/quantity-selector/quantity-selector.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { CartProduct } from 'src/app/modules/core/interfaces/cart-product-interface';
+
+import { CartService } from 'src/app/modules/core/services/cart.service';
 
 @Component({
   selector: 'app-quantity-selector',
   templateUrl: './quantity-selector.component.html',
-  styleUrls: ['./quantity-selector.component.scss']
+  styleUrls: ['./quantity-selector.component.scss'],
 })
 export class QuantitySelectorComponent {
+  @Input() productId: number = 0;
+  @Output() productAmountChange = new EventEmitter<number>();
+  productAmount: number = 1;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.productAmount =
+      this.cartService.findProduct(this.productId)?.amount ??
+      this.productAmount;
+
+    this.productAmountChange.subscribe(
+      (amount) => (this.productAmount = amount)
+    );
+  }
+
+  handleProductAmount(value: number) {
+    const newAmount = this.productAmount + value;
+
+    if (newAmount >= 1) {
+      this.cartService.editProduct(this.productId, newAmount);
+      this.productAmountChange.emit(newAmount);
+    }
+  }
+
+  emitAmountChange() {
+    this.productAmountChange.emit(this.productAmount);
+  }
 }
