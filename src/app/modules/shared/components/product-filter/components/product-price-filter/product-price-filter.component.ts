325f89e3c609019cb72570823bb993e28f971f97
Matheus Henrique <matheushenr941@gmail.com>
2023-05-02 22:22:27 -0300

    feat: product price filter - handle range values

--- src/app/modules/shared/components/product-filter/components/product-price-filter/product-price-filter.component.ts
+++ src/app/modules/shared/components/product-filter/components/product-price-filter/product-price-filter.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product-price-filter',
   templateUrl: './product-price-filter.component.html',
-  styleUrls: ['./product-price-filter.component.scss']
+  styleUrls: ['./product-price-filter.component.scss'],
 })
 export class ProductPriceFilterComponent {
+
+  valueRange: string = '';
+  startValueRange: number = 0;
+  endValueRange: number = 0;
+
+  @Output() priceRangeChange = new EventEmitter<string>();
+
   formatLabel(value: number): string {
     return '$' + value;
+  }
+  
+  onStartValueChange(event: any) {
+    this.startValueRange = event;
+    this.emitRangeValue();
+  }
+
+  onEndValueChange(event: any) {
+    this.endValueRange = event;
+    this.emitRangeValue();
+  }
+
+  emitRangeValue() {
+    this.valueRange = String(this.startValueRange + '.' + this.endValueRange);
+    this.priceRangeChange.emit(this.valueRange);
   }
 }
