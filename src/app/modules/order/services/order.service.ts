337709cf7a409f4d3441e001461a79de7d021032
Matheus Henrique <matheushenr941@gmail.com>
2023-05-04 23:48:25 -0300

    feat: added order service

---
+++ src/app/modules/order/services/order.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Order } from './../interfaces/order-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private apiUrl?: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+  }
+  
+  getOrderById(id: number): Observable<Order> {
+    return this.httpClient.get<Order>(this.apiUrl + '/Order/' + id);
+  }
+
+  getOrdersByCustomerId(id: number): Observable<Order[]>{
+    return this.httpClient.get<Order[]>(this.apiUrl + '/Order/customer/' + id);
+  }
+}
