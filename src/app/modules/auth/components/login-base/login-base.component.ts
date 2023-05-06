8162c5e22a157b5a335784d231cdde0b06edd4ba
Matheus Henrique <matheushenr941@gmail.com>
2023-05-05 23:55:11 -0300

    feat: login close button go back in navigation

--- src/app/modules/auth/components/login-base/login-base.component.ts
+++ src/app/modules/auth/components/login-base/login-base.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login-base',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class LoginBaseComponent {
 
+  constructor(private location: Location) {}
+
+  navigateBack() {
+    this.location.back();
+  }
 }
