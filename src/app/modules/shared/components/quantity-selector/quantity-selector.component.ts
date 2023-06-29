ac7952158de919c2441e5801f3b3b6a227bd27c5
Matheus Henrique <matheushenr941@gmail.com>
2023-06-28 21:27:49 -0300

    refactor: quantity selector more generic

--- src/app/modules/shared/components/quantity-selector/quantity-selector.component.ts
+++ src/app/modules/shared/components/quantity-selector/quantity-selector.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
-import { CartProduct } from 'src/app/modules/core/interfaces/cart-product-interface';
-
-import { CartService } from 'src/app/modules/core/services/cart.service';
 
 @Component({
   selector: 'app-quantity-selector',
@@ -9,38 +6,23 @@ import { CartService } from 'src/app/modules/core/services/cart.service';
   styleUrls: ['./quantity-selector.component.scss'],
 })
 export class QuantitySelectorComponent {
-  @Input() productId: number = 0;
-  @Output() productAmountChange = new EventEmitter<number>();
-  productAmount: number = 1;
+  @Output() quantityChange = new EventEmitter<number>();
+  @Input() quantity: number = 1;
 
-  constructor(private cartService: CartService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.productAmount =
-      this.cartService.findProduct(this.productId)?.amount ??
-      this.productAmount;
-
-    this.productAmountChange.subscribe(
-      (amount) => (this.productAmount = amount)
+    this.quantityChange.subscribe(
+      (quantity) => (this.quantity = quantity)
     );
   }
 
-  handleProductAmount(value: number) {
-    const newAmount = this.productAmount + value;
-
-    const cartProduct = {
-      id: this.productId,
-      amount: newAmount,
-      totalPrice: 0,
-    } as CartProduct;
-
-    if (newAmount >= 1) {
-      this.cartService.editProduct(cartProduct);
-      this.productAmountChange.emit(newAmount);
-    }
+  handleQuantity(value: number) {
+    const newQuantity = this.quantity + value;
+    if (newQuantity >= 1) this.quantityChange.emit(newQuantity);
   }
 
-  emitAmountChange() {
-    this.productAmountChange.emit(this.productAmount);
+  emitQuantityChange() {
+    this.quantityChange.emit(this.quantity);
   }
 }
