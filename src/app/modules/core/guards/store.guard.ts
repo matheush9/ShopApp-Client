8827a5fec3280030525d9d64e7287cc8f408d5da
Matheus Henrique <matheushenr941@gmail.com>
2023-07-05 22:10:46 -0300

    fix: store guard always false if refresh is triggered

--- src/app/modules/core/guards/store.guard.ts
+++ src/app/modules/core/guards/store.guard.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 
 import { StoreService } from '../../store/services/store.service';
 import { SnackbarService } from '../../shared/services/snackbar.service';
+import { JwtTokenService } from '../../shared/services/jwt-token.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +18,9 @@ import { SnackbarService } from '../../shared/services/snackbar.service';
 export class StoreGuard implements CanActivate {
   constructor(
     private storeService: StoreService,
-    private snackBarService: SnackbarService
+    private snackBarService: SnackbarService,
+    private jwtTokenService: JwtTokenService,
+    private router: Router
   ) {}
 
   canActivate(
@@ -27,10 +31,22 @@ export class StoreGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (this.storeService.currentStore) 
+
+    if (this.storeService.currentStore)
       return true;
-      
-    this.snackBarService.openSnackbar('Sorry, you need to have a store to acess this route...');
-    return false;
+
+    const userId = this.jwtTokenService.getAuthenticatedUserId();
+
+    return this.storeService.getStoreByUser(userId).pipe(
+      map(() => {
+        return true;
+      }),
+      catchError(() => {
+        this.snackBarService.openSnackbar(
+          'Sorry, you need to have a store to acess this route...'
+        );
+        return this.router.navigate(['']);  
+      })
+    );
   }
 }
