bb151d0b5e469c44cfa77c74eddfddddcf9522e2
Matheus Henrique <matheushenr941@gmail.com>
2023-07-08 11:53:17 -0300

    build: update prod environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://168.75.90.247/api',
-    imagesProviderUrl: 'http://168.75.90.247'
+    apiUrl: 'http://168.75.90.247:8001/api',
+    imagesProviderUrl: 'http://168.75.90.247:8001'
 };
